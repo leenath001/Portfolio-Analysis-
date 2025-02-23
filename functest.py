d3a519f2368266b64e52346bedabf5cbfe69128c
leenath001 <leenathaniel001@gmail.com>
2025-02-23 16:02:27 -0500

    Update functest.py
    Implementing VAR (x[5])

--- functest.py
+++ functest.py
@@ -3,18 +3,27 @@ import MC_funcs
 import matplotlib.pyplot as plt
 import seaborn
 
-iter = 1000
-days = 5
-paths = 5000
+iter = 500
+days = 30
+paths = 50000
 
-x = Analysis_funcs.portfolio_analysis(iter,days,paths,["NVDA", "AAPL"],[25,50])
+tick = ["VOO","TSPA"]
+alo = [250,400]
+
+x = Analysis_funcs.portfolio_analysis(iter,days,paths,tick,alo)
 port = x[0]
 initsum = x[1]
+print()
 print(x[2])
+print()
 print(x[3])
+print()
 print(x[4])
+print()
+print(x[5])
+print()
 
-MC_funcs.prob_tool(1,initsum+10,port,paths)
+MC_funcs.prob_tool(1,5000,port,paths)
 
 plt.figure()
 plt.hist(port,bins = 60, density = True)
@@ -22,7 +31,7 @@ seaborn.kdeplot(port)
 plt.axvline(x = initsum, c = 'lime')
 plt.xlabel('Price')
 plt.ylabel('Density')
-plt.axvline(x = 85, color = 'red')
+#plt.axvline(x = 85, color = 'red')
 plt.title('Price Distribution')
 plt.show()
 
